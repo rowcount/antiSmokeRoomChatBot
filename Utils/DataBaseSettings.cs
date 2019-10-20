b4d8e37edcacad02eff5bd08b44bd56f259f5329
rowcount <d.d.eliseev@gmail.com>
2019-10-20 23:58:18 +0300

    distribution by users and chat in the database

--- Utils/DataBaseSettings.cs
+++ Utils/DataBaseSettings.cs
@@ -9,13 +9,20 @@ namespace AntiSmokeRoomChatBot.Utils
     public class DataBaseSettings
     {
         private IMongoDatabase _database;
-        private Message _message;
 
         public void SaveToMongo(string conStr, MessageEventArgs mea)
         {
             var ChatId = mea.Message.Chat.Id.ToString();
+            string DatabaseName;
+            if (mea.Message.Chat.Id < 0)
+                DatabaseName = $"group_chat_{mea.Message.Chat.Title}";
+            else
+                if ( mea.Message.Chat.Username == null)
+                    DatabaseName = mea.Message.Chat.Id.ToString();
+                else    
+                    DatabaseName = mea.Message.Chat.Username;
             _database = new MongoClient(conStr)
-                .GetDatabase("antiSmokeRoomChatBot");
+                .GetDatabase(DatabaseName);
             var collection =_database.GetCollection<MessageEventArgs>(ChatId);
             collection.InsertOne(mea);
         }
